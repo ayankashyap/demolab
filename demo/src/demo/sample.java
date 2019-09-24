4feba1f22623d303c456a8fed312948ae2873273
ayankashyap <ayankashyap8@gmail.com>
2019-09-24 15:31:46 +0530

    added functionality to input 2 numbers and the desired operator

--- demo/src/demo/sample.java
+++ demo/src/demo/sample.java
@@ -1,7 +1,20 @@
 package demo;
 
+import java.util.Scanner;
+
 public class sample {
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+    	double num1, num2;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number:");
+
+        num1 = scanner.nextDouble();
+        System.out.print("Enter second number:");
+        num2 = scanner.nextDouble();
+
+        System.out.print("Enter an operator (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        scanner.close();
 	}
 }
